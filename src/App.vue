71a8f07616341e345fc4cd1b99290349e693d3f7
Jemma-MS <jemma.m_s@outlook.com>
2023-05-09 21:36:53 -0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
+  <NavBar />
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <router-view />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import { defineAsyncComponent } from 'vue';
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NavBar: defineAsyncComponent( () => import (/*  webpackChunkName: "NavBar" */ '@/modules/shared/components/NavBar') )
   }
 }
 </script>
---
+++ src/modules/router/router.js
@@ -0,0 +1,31 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+const routes = [
+    { 
+        path: '/',                    
+        component: () => import (/*  webpackChunkName: "AboutPage" */ '@/modules/pokemon/pages/ListPage') 
+    },
+    {   path: '/about',               
+        component: () => import (/*  webpackChunkName: "AboutPage" */ '@/modules/pokemon/pages/AboutPage') 
+    },
+    {   path: '/:id', 
+        name: 'pokemon-id', 
+        component: () => import (/*  webpackChunkName: "PokemonPage" */ '@/modules/pokemon/pages/PokemonPage'),
+        props: ( route ) => {
+            const id = Number ( route.params. id )
+            return isNaN( Number( id ) ) ? { id: 1 } : { id: Number( id ) }
+        }
+
+    },
+    {   
+        path: '/:pathMatch(.*)*',
+        component: () => import (/*  webpackChunkName: "NoFoundPage" */ '@/modules/shared/pages/NoPageFound')
+    },
+]
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes, 
+})
+
+export default router
